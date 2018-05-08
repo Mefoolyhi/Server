8c997123516030e8c2c265714628aa4ce4f46723
Gabdorahmanova Elena <mefoolyhi@gmail.com>
2018-05-08 17:03:04 +0500

    beginning

---
+++ src/main/java/com/example/admin/fence/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.admin.fence.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.example.admin.fence")
+public class WebConfig implements WebMvcConfigurer {
+
+
+}
---
+++ src/main/java/com/example/admin/fence/controller/NewsController.java
@@ -0,0 +1,21 @@
+package com.example.admin.fence.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/news")
+public class NewsController {
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getNews(ModelMap model){
+        return "Test";
+    }
+
+
+
+}
